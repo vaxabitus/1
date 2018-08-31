8b3e2eff00082275f56b621d3d61e390513674c5
root <admin@example.com>
2018-08-31 16:00:32 +0400

    Pipe

---
+++ test.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent {
+        // run with the custom python slave
+        // will dynamically provision a new pod on APPUiO
+        label 'nodejs'
+    }
+    
+        stage('Build') {
+            echo "Build"
+        sleep 5
+        }
+        stage('Test') {
+            echo "Test"
+        sleep 2
+        }
+    
+    
+        stage('DeployDev') {
+            echo "Deploy to Dev"
+        sleep 5
+        }
+        stage('PromoteTest') {
+            echo "Deploy to Test"
+        sleep 5
+        }
+        stage('PromoteProd') {
+            echo "Deploy to Prod"
+        sleep 5
+        }
+}
